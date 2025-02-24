6a37344128a3c365e568b73fb804b5e5a2ee04bf
sebastian espeset <sebastian.c.espeset@gmail.com>
2025-02-24 10:03:45 -0600

    adds dynamic input info for my-app

--- my-app/src/app/projects-home/projects-home.component.ts
+++ my-app/src/app/projects-home/projects-home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects-home',
   standalone: true,
   imports: [],
+  selector: 'app-projects-home',
   templateUrl: './projects-home.component.html',
-  styleUrl: './projects-home.component.css'
+  styleUrls: ['./projects-home.component.css'],
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit {
+  firstName: string = 'William';
+  lastName: string = 'Wallace';
+  phoneNumber: string = '(201) 748-6000';
+  jobTitle: string = 'senior assistant vice president';
 
+  setProperty(event: Event) {
+    let target = event.target as HTMLInputElement;
+    let property: string = target.id;
+    switch (property) {
+      case 'firstNameInput':
+        this.firstName = target.value;
+        break;
+      case 'lastNameInput':
+        this.lastName = target.value;
+        break;
+      case 'phoneNumberInput':
+        this.phoneNumber = target.value;
+        break;
+      case 'jobTitleInput':
+        this.jobTitle = target.value;
+        break;
+      default:
+        console.warn('No matching ID found!');
+        break;
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
